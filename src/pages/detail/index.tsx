15200538ea60821b4a4cb3f7497a9bfd22bcd16b
Natthaphon <npnk123@gmail.com>
2024-09-27 21:36:19 +0700

    add Back Button

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ReactLoading from "react-loading";
 import uuid from "react-uuid";
+import { Link } from "react-router-dom";
 
 type CountryType = {
   data: CountryItem[] | undefined;
@@ -66,6 +67,16 @@ const Detailpage = () => {
 
   return (
     <div className="w-[90%] m-[auto]">
+      <Link to={`/`}>
+      <div className="text-right p-4 mt-4 mr-10">
+                <button
+                  type="button"
+                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                >
+                  Back to main page
+                </button>
+              </div>
+      </Link>
       <div className="flex justify-center">
         {country.data &&
           country.data.map((item: CountryItem) => (
